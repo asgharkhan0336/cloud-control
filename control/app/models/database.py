7464450e25c46efb4cb6db0ada90999447388f08
asgharkhan0336 <45183486+asgharkhan0336@users.noreply.github.com>
2026-04-20 18:23:08 +0500

    Update database.py

--- control/app/models/database.py
+++ control/app/models/database.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, Boolean, DateTime, ForeignKey, DECIMAL
+from sqlalchemy import Column, Integer, String, Boolean, DateTime, ForeignKey, DECIMAL, Text
 from sqlalchemy.sql import func
 from sqlalchemy.orm import relationship
 from app.database import Base
