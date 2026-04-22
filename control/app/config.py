5a6703427f6c3ee01b325923fe6f71e5b27519c0
asgharkhan0336 <45183486+asgharkhan0336@users.noreply.github.com>
2026-04-22 14:17:46 +0500

    Update config.py

--- control/app/config.py
+++ control/app/config.py
@@ -24,7 +24,7 @@ class Settings(BaseSettings):
     # Network Settings
     vm_ip_pool_start: str = "192.168.122.100"
     vm_ip_pool_end: str = "192.168.122.200"
-    
+    console_host:str = "49.12.132.53:8000"
     class Config:
         env_file = ".env"
 
